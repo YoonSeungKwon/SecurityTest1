d142873d4b621ed8eb69d569a0498a3738276693
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2023-05-28 00:35:23 +0900

    Add files via upload

---
+++ src/test/java/yoon/test/security0525/Security0525ApplicationTests.java
@@ -0,0 +1,13 @@
+package yoon.test.security0525;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security0525ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
